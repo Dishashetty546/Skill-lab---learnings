7b9307318a61bba324e20cecf3d3283b8a88de45
dishashetty546 <dishashetty546@gmail.com>
2025-04-03 15:36:04 +0530

    updated

--- app/students/details/page.js
+++ app/students/details/page.js
@@ -1,8 +1,17 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function StudentDetails() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <StudentDetailsContent />
+    </Suspense>
+  );
+}
+
+function StudentDetailsContent() {
   const searchParams = useSearchParams();
 
   // Extract data from URL parameters
